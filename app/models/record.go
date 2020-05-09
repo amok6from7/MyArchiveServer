574f1c17b4941b15abdc2e75f2e167a9ba65ddc1
amok <lestang7@gmail.com>
2020-05-09 12:38:22 +0900

    add sort

--- app/models/record.go
+++ app/models/record.go
@@ -15,28 +15,28 @@ type RECORD struct {
 
 type Result struct {
 	gorm.Model
-	Title string `json:"title"`
-	Name  string `json:"name"`
+	Title      string `json:"title"`
+	Name       string `json:"name"`
 	Evaluation string `json:"evaluation"`
 }
 
 func FindById(id string) []Result {
 	var results []Result
 	db.Table("records").Where("records.ID = ?", id).Select("records.title, authors.name, records.evaluation").Joins("left join authors on authors.id = records.author").Scan(&results)
-	return results//TODO order by author name asc
+	return results
 }
 
 func FindByTitle(param string) []Result {
 	var results []Result
 	title := fmt.Sprintf("%%%s%%", param)
-	db.Table("records").Where("records.title LIKE ? OR records.title_kana LIKE ?", title, title).Select("records.title, authors.name, records.evaluation").Joins("left join authors on authors.id = records.author").Scan(&results)
-	return results//TODO order by author name asc
+	db.Table("records").Order("authors.name_kana asc").Where("records.title LIKE ? OR records.title_kana LIKE ?", title, title).Select("records.id, records.title, authors.name, records.evaluation").Joins("left join authors on authors.id = records.author").Scan(&results)
+	return results
 }
 
 func FindByAuthor(param string) []Result {
 	var results []Result
 	name := fmt.Sprintf("%%%s%%", param)
-	db.Table("records").Where("authors.name LIKE ? OR authors.name_kana LIKE ? ", name, name).Select("records.title, authors.name, records.evaluation").Joins("left join authors on authors.id = records.author").Scan(&results)
+	db.Table("records").Order("authors.name_kana asc, records.title_kana asc").Where("authors.name LIKE ? OR authors.name_kana LIKE ? ", name, name).Select("records.id, records.title, authors.name, records.evaluation").Joins("left join authors on authors.id = records.author").Scan(&results)
 	return results
 }
 
